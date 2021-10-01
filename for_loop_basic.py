82e6f9caeff28015a76e4af0869e3a3667cfeaf3
Francis Frago <72363705+fdrive422@users.noreply.github.com>
2021-10-01 13:10:35 -0700

    Update for_loop_basic.py

--- for_loop_basic.py
+++ for_loop_basic.py
@@ -9,11 +9,11 @@ for i in range(5, 1001, 5):
 # 3. Counting, the Dojo Way - Print integers 1 to 100. If divisible by 5, print "Coding" instead. If divisible by 10, print "Coding Dojo".
 for i in range(1, 101):
     if i % 10 == 0:
-        print('Coding')
-    elif i % 5 == 0:
         print('Coding Dojo')
+    elif i % 5 == 0:
+        print('Coding')
     else:
-        print(1)
+        print(i)
 
 # 4. Whoa. That Sucker's Huge - Add odd integers from 0 to 500,000, and print the final sum.
 sum = 0
